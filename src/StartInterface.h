525201fdef0f486a7a62b2de2bc8c8bc62a9449c
Sodacooky <523379653@qq.com>
2020-10-13 13:13:51 +0800

    开始界面

---
+++ src/StartInterface.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "TextChooser.h"
+
+//返回功能序号
+int StartInterface();
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 ﻿//程序从这里开始
 
-#include "TextChooser.h"
+#include <iostream>
+
+#include "StartInterface.h"
+
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+	cout << StartInterface();
 	return 0;
 }
